c882b67fd9e1b8b2b76d50b96c41b26b34b9d834
Meng Vang <meng.vang92@yahoo.com>
2019-02-06 18:42:48 -0600

    inner join on customer and orders table

--- orders.sql
+++ orders.sql
@@ -1,3 +1,6 @@
+-- When delting all records from table, this query will disable code 1175
+-- SET SQL_SAFE_UPDATES = 0;
+
 CREATE TABLE ordersTable
 (
 	orderId INT NOT NULL AUTO_INCREMENT,
@@ -12,6 +15,15 @@ SELECT * FROM ordersTable;
 
 ALTER TABLE ordersTable
 ADD orderNumber INT;
-ALTER TABLE orders
 
-SELECT firstName, lastName FROM customerTable;
+ALTER TABLE ordersTable
+MODIFY orderDate DATETIME DEFAULT CURRENT_TIMESTAMP;
+
+INSERT INTO ordersTable (customerId, productId, orderNumber)
+VALUES (1, 7, 001),
+(2, 1, 002),
+(3, 6, 003),
+(4, 5, 004),
+(5, 2, 005),
+(6, 4, 006),
+(7, 8, 007);
